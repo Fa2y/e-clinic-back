1fc0fd90231982d21795cd3f9f470bf99e0d856f
Fa2y <a.merzoukbenselloua@esi-sba.dz>
2021-06-01 15:56:34 +0100

    Changed requerment and Added auth app

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from auth.urls import urlpatterns as authUrls
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
+    path('', authUrls),
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
