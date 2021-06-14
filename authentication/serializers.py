4189009de981bb2b11aae3faed6fdf38801ed835
Fa2y <a.merzoukbenselloua@esi-sba.dz>
2021-06-14 19:33:09 +0100

    Added password confirmation and validation

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
+from allauth.account.adapter import get_adapter
 from .models import *
 
 
@@ -7,11 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
     User Model Serializer
     Excluded Fields: password, username, is_staff, user_permissions, groups
     '''
-    password = serializers.CharField(write_only=True, required=True)
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        exclude = ('username', 'is_staff',
+        exclude = ('username', 'password', 'is_staff',
                    'user_permissions', 'groups',)
 
 
@@ -27,9 +30,11 @@ class PatientSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
+        print("CREATING")
         user_data = validated_data.pop('user')
         user = User.objects.create(email=user_data.get('email'))
-        user.set_password(user_data.get('password'))
+        password = get_adapter().clean_password(user_data.get('password1'))
+        user.set_password(password)
         user.first_name = user_data.get('first_name', '')
         user.last_name = user_data.get('last_name', '')
         user.sex = user_data.get('sex', 'Male')
@@ -48,4 +53,9 @@ class PatientSerializer(serializers.ModelSerializer):
         return instance
 
     def validate(self, attrs):
+        print("VALIDATING")
+        user_data = attrs.get('user')
+        if user_data['password1'] != user_data['password2']:
+            raise serializers.ValidationError(
+                _("The two password fields didn't match."))
         return attrs
