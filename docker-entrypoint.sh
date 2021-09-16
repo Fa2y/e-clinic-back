7284a5130b5b0d14589a6dbaed7ba2af9a22df92
Fa2y <a.merzoukbenselloua@esi-sba.dz>
2021-09-16 01:19:06 +0100

    Fixed docker files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,13 @@
 cd $APP_PATH
 
 # check if the database is ready
-echo "Waiting for Postgres DB"
-while ! pg_isready -h $PG_HOSTNAME > /dev/null 2>&1;
-do
-  echo -n "."
-  sleep 1
-done
-echo "Postgres DB is ready"
+# echo "Waiting for Postgres DB"
+# while ! pg_isready -h $PG_HOSTNAME > /dev/null 2>&1;
+# do
+#   echo -n "."
+#   sleep 1
+# done
+# echo "Postgres DB is ready"
 
 # in case we used mysql db
 # echo "Waiting for Mysql DB"
@@ -22,26 +22,25 @@ echo "Postgres DB is ready"
 # create tables
 echo "Updating Database Tables"
 ./manage.py makemigrations
-./manage.py makemigrations legacy
+./manage.py makemigrations authentication appointment medical
 ./manage.py migrate
 echo "The Database has been updated"
 
 # load data to tables
 echo "Updating Database Tables"
-./manage.py loaddata seed_data.json
-./manage.py loaddata publictargets_data.json
+./manage.py loaddata data.json
 echo "The Database has been updated"
 
 # create roles
-echo "Groups..."
-cat /create_groups.py | ./manage.py shell
+# echo "Groups..."
+# cat /create_groups.py | ./manage.py shell
 # create admin user
-echo "Superuseres..."
-cat /create_superuser.py | ./manage.py shell
+# echo "Superuseres..."
+# cat /create_superuser.py | ./manage.py shell
 
 # collect static files
 ./manage.py collectstatic --no-input
 
 # run the server
 echo "Starting the server..."
-gunicorn backend.wsgi --bind 0.0.0.0:8000
+gunicorn api.wsgi --bind 0.0.0.0:8000
